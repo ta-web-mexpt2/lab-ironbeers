badae3d4ca4a97f3400cf8a12df3c9872c5c362f
Josafat Solis <jsolis@macroaplicaciones.com>
2020-04-12 20:29:37 -0500

    done with bonus and handlebars

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+const exphbr = require('express-handlebars');
 
 const app = express();
+
+// handlebars setup
+// https://github.com/ericf/express-handlebars/issues/195
+app.engine(
+  'handlebars',
+  exphbr({
+    defaultLayout: 'index',
+    layoutsDir: path.join(__dirname, 'views'),
+  })
+);
+app.set('view engine', 'handlebars');
+//app.set('views', './views');
 
 // https://www.npmjs.com/package/punkapi-javascript-wrapper
 const punkAPI = new PunkAPIWrapper();
@@ -40,12 +53,14 @@ function beersFiltered(beers, sliceFirstNotLastFive) {
 }
 
 function findAndSendBeers(res, sliceFirstNotLastFive, msgWhenFinish) {
-  punkAPI.getBeers().then((beers) => {
-    return beersFiltered(beers, sliceFirstNotLastFive);
-  })
-  .then((beers) => {
-    res.send({ message: msgWhenFinish, beers });
-  });
+  punkAPI
+    .getBeers()
+    .then((beers) => {
+      return beersFiltered(beers, sliceFirstNotLastFive);
+    })
+    .then((beers) => {
+      res.send({ message: msgWhenFinish, beers });
+    });
 }
 
 app.get('/beers', (req, res, next) => {
@@ -57,16 +72,58 @@ app.get('/first_five', (req, res, next) => {
 });
 
 app.get('/last_five', (req, res, next) => {
-  findAndSendBeers(res, false, 'Last five retrieved successfully')
+  findAndSendBeers(res, false, 'Last five retrieved successfully');
 });
 
 // /random-beer
 app.get('/random-beer', (req, res, next) => {
-  punkAPI.getRandom((beer) => {
-    
-  });
+  punkAPI
+    .getRandom((beer) => {
+      return new Promise((resolve, reason) => {
+        resolve(beer);
+      });
+    })
+    .then((beer) => {
+      console.log(beer);
+      // beer is an array
+      res.render('index', { ...beer[0] });
+    })
+    .catch((reason) => {
+      console.log(`Error: ${reason}`);
+    });
+});
+
+//filter_date
+// app.get('/filter_date', (req, res) => {
+//   punkAPI.getBeers({brewed_before: '01-2010'})
+//   .then((beers) => {
+//     return new Promise((resolve, reason) => {
+//       resolve(beers);
+//     });
+//   }).then((beers) => {
+//     res.send(beers);
+//   });
+// });
+
+app.get('/filter_date', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then((beers) => {
+      return new Promise((resolve, reason) => {
+        resolve(beers);
+      });
+    })
+    .then((beers) => {
+      res.send(
+        // format of first_brewed: mm/yyyy
+        beers.filter((beer) => parseInt(beer.first_brewed.split('/')[1]) < 2010)
+      );
+    })
+    .catch((reason) => {
+      console.log(`Error: ${reason}`);
+    });
 });
 
 app.listen(3000, () => {
-  console.log('Listo para recibir peticiones en localhost:3000 ...');
+  console.log('Ready for requests on localhost:3000 ...');
 });
