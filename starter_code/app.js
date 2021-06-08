958cea393541611ebd537382c8ca2680fcafc77b
luisve9 <luissamdoval.9@gmail.com>
2021-06-08 00:09:35 -0500

    final

--- starter_code/app.js
+++ starter_code/app.js
@@ -11,5 +11,54 @@ const punkAPI = new PunkAPIWrapper();
 app.get('/', (req, res, next) => {
   res.status(200).json({message:"todo esta bien"})
 });
+app.get('/beers', (req,res,next) => {
+  punkAPI
+    .getBeers()
+    .then(beers => {res.json(beers)})
+    .catch(error => {
+      console.log(error);
+    });
+
+})
+
+app.get('/beers/first_five', (req,res,next) => {
+  punkAPI
+    .getBeers()
+    .then(beers => {res.json(beers.slice(0, 5))})
+    .catch(error => {
+      console.log(error);
+    });
+})
+
+app.get('/beers/last_five', (req,res,next) => {
+  punkAPI
+    .getBeers()
+    .then(beers => {res.json(beers.slice(Math.max(beers.length - 5, 0)))})
+    .catch(error => {
+      console.log(error);
+    });
+})
+
+app.get('/beers/random_beer', (req,res,next) => {
+  punkAPI
+    .getRandom()
+    .then(beers => {res.json(beers)})
+    .catch(error => {
+      console.log(error);
+    });
+})
+
+app.get('/beers/filter_date', (req,res,next) => {
+  punkAPI
+    .getBeers()
+    .then(beers => {res.json(beers.filter(beer => {
+      var year = beer.first_brewed.slice(3, 7)
+      return Number(year) < 2010
+    }))})
+    .catch(error => {
+      console.log(error);
+    });
+
+})
 
 app.listen(3000);
